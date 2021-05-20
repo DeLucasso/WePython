840fea709d2abe29ef9583e1512ec86a0202b3e7
LuCaS <dumdumcz@gmail.com>
2021-05-20 00:11:39 -0700

    Update RollercosterTickets.py

--- 100 days of Python/Day3/RollercosterTickets.py
+++ 100 days of Python/Day3/RollercosterTickets.py
@@ -1,11 +1,3 @@
-# Pyhon basics (nested conditions) : if > if/else > else 
-# Rollercoster ticketing system
-
-#__author__ = "DeLucasso"
-#__copyright__ = "Copyright (c) 2020 DeLucasso"
-#__license__ = "Public Domain"
-#__version__ = "1.0"
-
 print("Welcome to the rollercoaster!")
 height = int(input("What's your height in cm? "))
 
@@ -13,10 +5,17 @@ if height >= 120:
   print("Awesome! You can ride with us!")
   age = int(input("What's your age? "))
   if age < 12:
-    print("Please pay $5")
-  elif age < 18:
-    print("Please pay $7")
+    price = 5
+    print(f"Child tickets are {price}")
+  elif age <= 18:
+    price = 7
+    print(f"Youth Tickets are ${price}")
   else:
-    print("Please pay $12")
+    price = 12
+    print(f"Adult tickets are ${price}")
+  photos = input("Do you want photos for extra $3? Y / N ? ")
+  if photos == "Y" or photos == "y":
+    price += 3
+    print(f"Your ticket cost {price}")
 else:
     print("You have to grow up, sorry, you can't ride yet!")
