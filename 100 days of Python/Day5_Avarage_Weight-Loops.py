cbe137aca229780ef321169c2091a7e0c86543eb
LuCaS <dumdumcz@gmail.com>
2021-06-30 21:27:01 -0700

    Create Day5_Avarage_Weight-Loops.py

---
+++ 100 days of Python/Day5_Avarage_Weight-Loops.py
@@ -0,0 +1,16 @@
+student_heights = input("Input a list of student heights like : 200 230 203 196 etc. : ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+# You can't use:
+# print (sum(student_heights))
+# print (len(student_heights))
+
+len_counter = 0
+sum_heights = 0
+for height in student_heights:
+  len_counter += 1
+  sum_heights = int(height) + sum_heights
+print(f"Total sum of heights are : {sum_heights}")
+print(f"Number of heights = {len_counter}")
+print(f"Average weight is : {sum_heights / len_counter}")
